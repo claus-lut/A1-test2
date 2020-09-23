2c3947246ca13fd7067980f2c58a6d1169e651bf
claus-lut <71755294+claus-lut@users.noreply.github.com>
2020-09-23 13:14:42 +0300

    Changed th tag to td

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ tabletext += "<table>\n";
 for (i = 0; i < 5; i++) {
   tabletext += "<tr>\n";
   for (j = 0; j < 5; j++) {
-    tabletext += "<th>    </th>";
+    tabletext += "<td>    </td>";
   }
   tabletext += "</tr>\n";
 }
