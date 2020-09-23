6fd30747d03acdab4129b899a66f85e4c322617d
claus-lut <71755294+claus-lut@users.noreply.github.com>
2020-09-23 14:29:50 +0300

    Added checks if player won

--- src/index.js
+++ src/index.js
@@ -1,7 +1,61 @@
 var tabletext = "";
 var i, j;
+var cursor = "o";
+var player = 1;
 
-tabletext += "<table>\n";
+function cellClick() {
+  if (this.innerHTML !== "o" && this.innerHTML !== "x") {
+    this.innerHTML = cursor; // The function returns the product of p1 and p2
+    checkWinner();
+    if (cursor == "o") {
+      cursor = "x";
+      player = 2;
+    } else {
+      cursor = "o";
+      player = 1;
+    }
+  }
+}
+
+function checkWinner() {
+  var table = document.getElementById("ttttable");
+  // Check columns
+  for (var i = 0; i < 5; i++) {
+    var winner = true;
+    for (var j = 0; j < 5; j++) {
+      if (table.rows[i].cells[j].innerHTML !== cursor) {
+        winner = false;
+      }
+    }
+    if (winner) {
+      alert("Player " + player + " won!");
+    }
+  }
+  // Check rows
+  for (var i = 0; i < 5; i++) {
+    var winner = true;
+    for (var j = 0; j < 5; j++) {
+      if (table.rows[j].cells[i].innerHTML !== cursor) {
+        winner = false;
+      }
+    }
+    if (winner) {
+      alert("Player " + player + " won!");
+    }
+  }
+  // Check diagonal
+  for (var i = 0; i < 5; i++) {
+    var winner = true;
+    if (table.rows[i].cells[i].innerHTML !== cursor) {
+      winner = false;
+    }
+  }
+  if (winner) {
+    alert("Player " + player + " won!");
+  }
+}
+
+tabletext += "<table id=ttttable>\n";
 for (i = 0; i < 5; i++) {
   tabletext += "<tr>\n";
   for (j = 0; j < 5; j++) {
@@ -12,3 +66,10 @@ for (i = 0; i < 5; i++) {
 tabletext += "</table>\n";
 
 document.getElementById("board").innerHTML = tabletext;
+
+var table = document.getElementById("ttttable");
+for (var i = 0, row; (row = table.rows[i]); i++) {
+  for (var j = 0, col; (col = row.cells[j]); j++) {
+    col.addEventListener("click", cellClick, false);
+  }
+}
